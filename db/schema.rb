616d2b99247ec2d9c7b9ed330fb3bfa2c573b985
Martin Turner <martin@coremelt.com>
2017-10-06 17:24:39 +0700

    Added subjects

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006050736) do
+ActiveRecord::Schema.define(version: 20171006083757) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.string "surname"
     t.string "student_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.string "subject_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
